28d87247a267fa79303c7223db557e664804a7cf
Valerie Russell <vruss14@gmail.com>
2021-05-31 23:01:16 -0400

    created basic function to retrieve search form info and redirect page

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Page loaded!");
+let searchInput = document.getElementById("search-input");
+let searchFilter = document.getElementById("search-filter");
+let searchBtn = document.getElementById("search-btn");
+
+function handleSearch(event) {
+    event.preventDefault();
+
+    let userSearch = searchInput.value;
+    let userFilter = searchFilter.value;
+
+    // To change the page to the search page
+    // window.location.href = "./search.html";
+
+    console.log(userSearch);
+    console.log(userFilter);
+
+}
+
+searchBtn.addEventListener("click", handleSearch);
