d73ef2d33292a26ab417c7d2158b99a31f2b1e40
Valerie Russell <vruss14@gmail.com>
2021-06-01 13:14:58 -0400

    redirect user to search page with search parameters in URL

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
+let searchForm = document.getElementById("search-form");
 let searchInput = document.getElementById("search-input");
 let searchFilter = document.getElementById("search-filter");
-let searchBtn = document.getElementById("search-btn");
 
 function handleSearch(event) {
     event.preventDefault();
 
     let userSearch = searchInput.value;
     let userFilter = searchFilter.value;
+    let userQuery = `./search.html?q=${userSearch}&format=${userFilter}`;
 
-    // To change the page to the search page
-    // window.location.href = "./search.html";
-
-    console.log(userSearch);
-    console.log(userFilter);
+    // To change the page to the search page (with the user's search parameters)
+    location.assign(userQuery);
 
 }
 
-searchBtn.addEventListener("click", handleSearch);
+searchForm.addEventListener("submit", handleSearch);
